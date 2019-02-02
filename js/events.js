3fe530bce350eae4bd3a401e614d07683efb5a42
Ale Encender <alejandromarzano@encendercomunicacion.com>
2019-02-02 14:24:46 -0300

    age modal

--- js/events.js
+++ js/events.js
@@ -1,15 +1,3 @@
-// var stickyOffset = $("#sticky-bar").offset();
-// var $contentDivs = $("#py");
-// $(document).ready(function () {
-//     $contentDivs.each(function (k) {
-//         var _thisOffset = $(this).offset();
-//         var _actPosition = _thisOffset.top - $(window).scrollTop();
-//         if (_actPosition < stickyOffset.top && _actPosition + $(this).height() > 0) {
-//             $("#sticky-bar").removeClass("rojo blanco").addClass($(this).hasClass("top") ? "rojo" : "blanco");
-//             return false;
-//         }
-//     });
-// });
 
 $(function () {
     var sticky = $("#sticky-bar");
@@ -23,3 +11,7 @@ $(function () {
         }
     });
 });
+
+
+$('#age').modal('show');
+
